789ae75fd42fadac77bcb6cfc93c1f7df18e823d
vitor-2006 <vitorbalbinotdellatorre@gmail.com>
2025-12-10 08:35:25 -0300

    arrumando rotas

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -33,7 +33,7 @@ export default {
 
   async get(req, res, next) {
     try {
-      const user = await userService.getUser(req.params.id);
+      const user = await userService.getUser(req.userId);
       return res.status(200).json(user);
     } catch (error) {
       next(error);
