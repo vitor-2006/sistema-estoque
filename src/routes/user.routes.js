61dcb7ed8f24de47a18aad4feecde8a67d67fb26
vitor-2006 <vitorbalbinotdellatorre@gmail.com>
2025-12-16 07:46:28 -0300

    tirando rota admin

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -8,8 +8,8 @@ const userRouter = express.Router();
 userRouter.post('/register', userController.create);
 userRouter.post('/login', userController.login);
 userRouter.get('/users', authMiddleware(), requireRole("ADMIN"), userController.list);
-userRouter.get('/user/', authMiddleware(), ensureValidId, userController.get);
-userRouter.put('/edit/', authMiddleware(), ensureValidId, userController.update);
-userRouter.delete('/delete/', authMiddleware(), ensureValidId, userController.remove);
+userRouter.get('/user/', authMiddleware(), userController.get);
+userRouter.put('/edit/', authMiddleware(), userController.update);
+userRouter.delete('/delete/', authMiddleware(), userController.remove);
 
 export default userRouter;
